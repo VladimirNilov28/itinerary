a62a5f4997594cef364a3e946f9c48e0b9ed339b
VladimirNilov28 <nilov.vova28@gmail.com>
2025-04-25 15:42:11 +0300

    another fix

--- src/main/java/org/example/prettifier/itinerary/services/FileFormaterController.java
+++ src/main/java/org/example/prettifier/itinerary/services/FileFormaterController.java
@@ -22,80 +22,87 @@ public class FileFormaterController {
         DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd MMM yyyy", Locale.ENGLISH);
         DateTimeFormatter t12Formatter = DateTimeFormatter.ofPattern("hh:mma (XXX)", Locale.ENGLISH);
         DateTimeFormatter t24Formatter = DateTimeFormatter.ofPattern("HH:mm (XXX)", Locale.ENGLISH);
+
         try (BufferedReader reader = new BufferedReader(new FileReader(link.getINPUT_FILE()));
              BufferedWriter writer = new BufferedWriter(new FileWriter(link.getOUTPUT_FILE()))) {
+
             String line;
             int line_num = 0;
             int empty_line = 0;
             boolean is_city_need = false;
+
             while ((line = reader.readLine()) != null) {
                 line_num++;
                 stats.appendLines(1);
+
                 if (line.trim().isEmpty()) {
                     empty_line++;
                     if (empty_line > 1) {
                         continue; // пропускаем лишнюю пустую строку
                     }
                 } else {
-                    empty_line = 0; // сброс, когда встретили непустую строку
+                    empty_line = 0;
                 }
+
+                // Заменяем \v, \f, \r на реальные переводы строки
                 line = line
                         .replace("\\v", "\n")
                         .replace("\\f", "\n")
                         .replace("\\r", "\n")
-                        .replaceAll("[ ]*\n[ ]*", "\n")
-                        .trim();
-                for (int i = 0; i < line.length(); i++) {
-                    if (i + 1 < line.length() && line.charAt(i) == '#' && line.charAt(i + 1) != '#') {
-                        is_city_need = (i > 0 && line.charAt(i - 1) == '*');
-                        //code_type = "IATA";
-                        if (i + 4 <= line.length()) {
-                            String airport_code = line.substring(i + 1, i + 4);
-                            writer.write(isCityNeed(airport_code, is_city_need));
-                            i += 4;
-                            stats.appendAirportsResolved(1);
-                        }
-                    } else if (line.startsWith("##", i)) {
-                        is_city_need = (i > 0 && line.charAt(i - 1) == '*');
-                        //code_type = "ICAO";
-                        if (i + 6 <= line.length()) {
-                            String airport_code = line.substring(i + 2, i + 6);
-                            writer.write(isCityNeed(airport_code, is_city_need));
-                            i += 6;
-                            stats.appendAirportsResolved(1);
-                        }
-                    } else if (startsWithAny(line, i, "T24", "T12", "D") != null) {
-                        String date_time_format = startsWithAny(line, i, "T24", "T12", "D");
-                        int start = line.indexOf('(', i);
-                        int end = line.indexOf(')', start);
-                        try {
-                            ZonedDateTime date_time = (start != -1 && end != -1 && start < end)
-                                    ? ZonedDateTime.parse(line.substring(start + 1, end))
-                                    : null;
-                            switch (String.valueOf(date_time_format)) {
-                                case "T24" -> writer.write(date_time.format(t24Formatter).replace("Z", "+00:00"));
-                                case "T12" -> writer.write(date_time.format(t12Formatter).replace("Z", "+00:00"));
-                                case "D" -> writer.write(date_time.format(dateFormatter));
-                                default -> writer.write("");
+                        .replaceAll("([ ]*\n[ ]*){2,}", "\n\n");
+
+                // Разбиваем строку по \n
+                String[] sublines = line.split("\n", -1);
+
+                for (String subline : sublines) {
+                    subline = subline.strip();
+                    for (int i = 0; i < subline.length(); i++) {
+                        if (i + 1 < subline.length() && subline.charAt(i) == '#' && subline.charAt(i + 1) != '#') {
+                            is_city_need = (i > 0 && subline.charAt(i - 1) == '*');
+                            if (i + 4 <= subline.length()) {
+                                String airport_code = subline.substring(i + 1, i + 4);
+                                writer.write(isCityNeed(airport_code, is_city_need));
+                                i += 4;
+                                stats.appendAirportsResolved(1);
                             }
-
-                        } catch (DateTimeParseException e) {
-                            writer.write(date_time_format + "(" + line.substring(start + 1, end) + ")");
+                        } else if (subline.startsWith("##", i)) {
+                            is_city_need = (i > 0 && subline.charAt(i - 1) == '*');
+                            if (i + 6 <= subline.length()) {
+                                String airport_code = subline.substring(i + 2, i + 6);
+                                writer.write(isCityNeed(airport_code, is_city_need));
+                                i += 6;
+                                stats.appendAirportsResolved(1);
+                            }
+                        } else if (startsWithAny(subline, i, "T24", "T12", "D") != null) {
+                            String date_time_format = startsWithAny(subline, i, "T24", "T12", "D");
+                            int start = subline.indexOf('(', i);
+                            int end = subline.indexOf(')', start);
+                            try {
+                                ZonedDateTime date_time = (start != -1 && end != -1 && start < end)
+                                        ? ZonedDateTime.parse(subline.substring(start + 1, end))
+                                        : null;
+                                switch (date_time_format) {
+                                    case "T24" -> writer.write(date_time.format(t24Formatter).replace("Z", "+00:00"));
+                                    case "T12" -> writer.write(date_time.format(t12Formatter).replace("Z", "+00:00"));
+                                    case "D" -> writer.write(date_time.format(dateFormatter));
+                                    default -> writer.write("");
+                                }
+                            } catch (DateTimeParseException e) {
+                                writer.write(date_time_format + "(" + subline.substring(start + 1, end) + ")");
+                            }
+                            i = end;
+                            stats.appendDatesReformatted(1);
+                        } else {
+                            if (subline.charAt(i) == '*' && i + 1 < subline.length() && subline.charAt(i + 1) == '#') continue;
+                            writer.write(Character.toString(subline.charAt(i)));
                         }
-                        i = end;
-                        stats.appendDatesReformatted(1);
-                    } else {
-                        if (line.charAt(i) == '*' && i + 1 < line.length() && line.charAt(i + 1) == '#') continue;
-                        writer.write(Character.toString(line.charAt(i)));
                     }
+                    writer.newLine(); // Перенос строки после каждой подстроки
                 }
-
-                writer.newLine();
-
-
             }
         }
     }
+
 
     private String isCityNeed (String airport_code, boolean is_city_need) {
         return (is_city_need)
